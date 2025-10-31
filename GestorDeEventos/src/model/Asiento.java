3a456c2a6e103384bac32e3206f6f5f2e1b8fa21
valentin <valentinfv@outlook.com>
2025-10-31 10:53:24 -0300

    Asiento

--- GestorDeEventos/src/model/Asiento.java
+++ GestorDeEventos/src/model/Asiento.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Asiento {
+
+int numero;
+boolean disponible;
+
+    public Asiento(int numero, boolean disponible) {
+        this.numero = numero;
+        this.disponible = disponible;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 }
